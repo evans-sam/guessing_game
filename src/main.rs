2b774a4daa1cd6cdba02046f25de5004d47288e8
Sam Evans <evans.sam.t@gmail.com>
2022-10-23 17:11:18 -0600

    Guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::cmp::Ordering;
+use std::io;
+
+use rand::{Rng, thread_rng};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = thread_rng().gen_range(0..100);
+    println!("The secret number is {secret_number}");
+
+    println!("Please input your guess.");
+
+    loop {
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        let guess: u32 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
 }
